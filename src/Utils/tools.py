2ee05a6329cb2a3f7aad568c351eedc2a441519a
xasdwa2 <136150400+xasdwa2@users.noreply.github.com>
2023-06-16 07:25:33 -0500

    Update tools.py

--- src/Utils/tools.py
+++ src/Utils/tools.py
@@ -4,19 +4,19 @@ import requests
 import pandas as pd
 
 games_header = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/57.0.2987.133 Safari/537.36',
     'Dnt': '1',
     'Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'en',
-    'origin': 'http://stats.nba.com',
+    'origin': 'http://www.mlb.com',
     'Referer': 'https://github.com'
 }
 
 data_headers = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Host': 'stats.mlb.com',
+    'Host': 'statsapi.mlb.com',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
     'Referer': 'https://www.mlb.com/',
@@ -37,23 +37,22 @@ def get_json_data(url):
 def get_todays_games_json(url):
     raw_data = requests.get(url, headers=games_header)
     json = raw_data.json()
-    return json
+    return json.get('dates', [])
 
 
 def to_data_frame(data):
     try:
-        data_list = data['rowSet']
+        return pd.DataFrame(data)
     except Exception as e:
         print(e)
-        return pd.DataFrame(data={})
-    return pd.DataFrame(data=data_list, columns=data['headers'])
+        return pd.DataFrame()
 
 
 def create_todays_games(input_list):
     games = []
     for game in input_list:
-        home_team = game['home_team_name']
-        away_team = game['away_team_name']
+        home_team = game['teams']['home']['team']['name']
+        away_team = game['teams']['away']['team']['name']
         games.append([home_team, away_team])
     return games
 
@@ -67,6 +66,5 @@ def create_todays_games_from_odds(input_dict):
 
 
 def get_date(date_string):
-    year1, month, day = re.search(r'(\d+)-\d+-(\d\d)(\d\d)', date_string).groups()
-    year = year1 if int(month) > 8 else int(year1) + 1
+    year, month, day = re.search(r'(\d+)-(\d+)-(\d+)', date_string).groups()
     return datetime.strptime(f"{year}-{month}-{day}", '%Y-%m-%d')
