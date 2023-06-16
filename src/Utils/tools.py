c731f7f73e98a3ae60cc30006f3b7731682bfd4f
xasdwa2 <136150400+xasdwa2@users.noreply.github.com>
2023-06-16 07:24:35 -0500

    Update tools.py

--- src/Utils/tools.py
+++ src/Utils/tools.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import re
 import requests
 import pandas as pd
-from .Dictionaries import team_index_current
 
 games_header = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -17,13 +16,12 @@ games_header = {
 data_headers = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Host': 'stats.nba.com',
+    'Host': 'stats.mlb.com',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Referer': 'https://www.nba.com/',
+    'Referer': 'https://www.mlb.com/',
     'Connection': 'keep-alive'
 }
-
 
 
 def get_json_data(url):
@@ -33,31 +31,29 @@ def get_json_data(url):
     except Exception as e:
         print(e)
         return {}
-    return json.get('resultSets')
+    return json
 
 
 def get_todays_games_json(url):
     raw_data = requests.get(url, headers=games_header)
     json = raw_data.json()
-    return json.get('gs').get('g')
+    return json
 
 
 def to_data_frame(data):
     try:
-        data_list = data[0]
+        data_list = data['rowSet']
     except Exception as e:
         print(e)
         return pd.DataFrame(data={})
-    return pd.DataFrame(data=data_list.get('rowSet'), columns=data_list.get('headers'))
+    return pd.DataFrame(data=data_list, columns=data['headers'])
 
 
 def create_todays_games(input_list):
     games = []
     for game in input_list:
-        home = game.get('h')
-        away = game.get('v')
-        home_team = home.get('tc') + ' ' + home.get('tn')
-        away_team = away.get('tc') + ' ' + away.get('tn')
+        home_team = game['home_team_name']
+        away_team = game['away_team_name']
         games.append([home_team, away_team])
     return games
 
@@ -66,12 +62,11 @@ def create_todays_games_from_odds(input_dict):
     games = []
     for game in input_dict.keys():
         home_team, away_team = game.split(":")
-        if home_team not in team_index_current or away_team not in team_index_current:
-            continue
         games.append([home_team, away_team])
     return games
 
+
 def get_date(date_string):
-    year1,month,day = re.search(r'(\d+)-\d+-(\d\d)(\d\d)', date_string).groups()
+    year1, month, day = re.search(r'(\d+)-\d+-(\d\d)(\d\d)', date_string).groups()
     year = year1 if int(month) > 8 else int(year1) + 1
     return datetime.strptime(f"{year}-{month}-{day}", '%Y-%m-%d')
